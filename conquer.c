8983e5088aa20a357cf0abd78fb9778e124acd5c
Insub Kim <inskim@c2r7s2.42seoul.kr>
2022-12-06 15:43:09 +0900

    2

--- conquer.c
+++ conquer.c
@@ -1,31 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   merge_sort.c                                       :+:      :+:    :+:   */
+/*   conquer.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inskim <inskim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 08:02:44 by inskim            #+#    #+#             */
-/*   Updated: 2022/12/06 15:34:56 by inskim           ###   ########.fr       */
+/*   Updated: 2022/12/06 15:43:00 by inskim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    merge_sort(t_deque *a, t_deque *b)
+void    conquer(t_deque *a, t_deque *b, t_model *model)
 {
-    t_model    *model;
+    t_model *next;
+    int     i;
+    int     flag;
+
+    next = model -> next;
+    //pop [0] from -> to
+    //cmp [0] [1] [2] and pop
     
-    model = find_model(a -> size);
-    //divide
-    model = get_last_node(model);
-    divide(a, b, model);
-    //conquer
-    //apply stack b|a into model's next node
-    model -> prev;
-    while (model -> prev)
+
+    //until div_count
+    i = 0;
+    if (a -> size)
+        flag = 0;
+    else
+        flag = 1;
+    while (i < next -> div_count)
     {
-        conquer(a, b, model);
-        model = model -> prev;
-    }    
+        pop_chunk(a, b, flag, next -> )
+        i++;
+    }
 }
