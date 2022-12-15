8238975133e375979f96a5dd0b03f1b559c9f8da
Insub Kim <inskim@c3r2s7.42seoul.kr>
2022-12-15 09:16:59 +0900

    22 12 15

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ while [ $number -le 100 ]
 do
 	echo "test" $number
 	export ARG=$(./push_swap_tester/random_numbers 100 -2147483648)
- 	./push_swap $ARG | ./checker $ARG | grep -E 'KO|Error'
-	./push_swap $ARG | ./checker_Mac $ARG | grep -E 'KO|Error'
-	
+ 	./push_swap $ARG | ./checker_Mac $ARG | grep -E 'KO|Error'
+	 echo ""
+	./push_swap $ARG | wc -l 
 	((number++))
 done
